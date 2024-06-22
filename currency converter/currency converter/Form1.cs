cb4255f18922d442904dd06dd8b9d58c9dc6d51d
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-22 13:54:10 +0300

    first

---
+++ currency converter/currency converter/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace currency_converter
+{
+    public partial class currency : Form
+    {
+        public currency()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.google.com/search?q=usd+try");
+        }
+    }
+}
